fabbe6f54abe22f91599572668cfeef7a9cb68f7
Waldon Chen <waldonchen@gmail.com>
2019-01-17 18:06:18 +0800

    used xelatex to compile latex babel

--- layers/waldon-org/packages.el
+++ layers/waldon-org/packages.el
@@ -447,7 +447,28 @@
 
       (setq org-latex-default-class "ctexart")
       (setq org-latex-pdf-process
-            '("latexmk -pdflatex='xelatex -interaction nonstopmode' -pdf -bibtex -outdir='%o' -f %f"))
+            '("latexmk -pdflatex='%latex -interaction nonstopmode' -pdf -bibtex -outdir='%o' -f %f"))
+
+      ;; use xelatex to compile babel
+      (setq org-format-latex-header "% xelatex
+\\documentclass[12pt]{article}
+\\usepackage[usenames]{color}
+\[PACKAGES]
+\[DEFAULT-PACKAGES]
+\\pagestyle{empty}             % do not remove
+% The settings below are copied from fullpage.sty
+\\setlength{\\textwidth}{\\paperwidth}
+\\addtolength{\\textwidth}{-3cm}
+\\setlength{\\oddsidemargin}{1.5cm}
+\\addtolength{\\oddsidemargin}{-2.54cm}
+\\setlength{\\evensidemargin}{\\oddsidemargin}
+\\setlength{\\textheight}{\\paperheight}
+\\addtolength{\\textheight}{-\\headheight}
+\\addtolength{\\textheight}{-\\headsep}
+\\addtolength{\\textheight}{-\\footskip}
+\\addtolength{\\textheight}{-3cm}
+\\setlength{\\topmargin}{1.5cm}
+\\addtolength{\\topmargin}{-2.54cm}")
 
       (setq org-latex-listings t)
 
@@ -468,13 +489,15 @@
          (js . nil)
          (plantuml . nil)
          (ditaa . nil)))
+
+      (setq org-confirm-babel-evaluate nil)
+
+      (require 'ox-extra)
+      (ox-extras-activate '(ignore-headlines))
+
+      (add-hook 'org-export-before-parsing-hook
+                'waldon-org/org-latex-header-blocks-filter)
       )
-
-    (require 'ox-extra)
-    (ox-extras-activate '(ignore-headlines))
-
-    (add-hook 'org-export-before-parsing-hook
-              'waldon-org/org-latex-header-blocks-filter)
     )
   )
 
